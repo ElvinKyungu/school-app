02ac7e3bc8f0fe91b150d9b9a2f625a4c226bbd8
Jonathan YOMBO <jonathanyombo@din-technology.com>
2022-06-07 14:04:41 +0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,9 @@
     # navigate into the build output directory
     cd dist
 
-    # if you are deploying to a custom domain
-    # echo 'www.example.com' > CNAME
 
     git init
-    git checkout -b main
+    git checkout -b gh-pages
     git add -A
     git commit -m 'deploy'
 
