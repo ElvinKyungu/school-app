630d5e7482f35bb5fff4f3e558d3784ed7762cf8
elvinkyungu <elvinkyungu.75@gmail.com>
2022-06-07 11:42:04 +0200

    add dist for the deploiement

---
+++ dist/assets/AboutView.c99f9fdd.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as t}from"./index.d9cd8013.js";const a={},c={class:"about"},_=t("h1",null,"This is an about page",-1),n=[_];function r(i,d){return o(),s("div",c,n)}var u=e(a,[["render",r]]);export{u as default};
