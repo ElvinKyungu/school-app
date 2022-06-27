90541e642d8bbc7048cabcc2b7cf0760e63b47d2
elvinkyungu <elvinkyungu.75@gmail.com>
2022-06-27 16:35:02 +0200

    supression de la page dashboard

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <script setup>
-import NavBar from '@/components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
-import HomeAbout from '@/components/HomeAbout.vue'
-import CategorY from '@/components/Category.vue'
-import Category from '../components/Category.vue';
+    import NavBar from '@/components/NavBar.vue'
+    import Footer from '@/components/Footer.vue'
+    import HomeAbout from '@/components/HomeAbout.vue'
+    import CategorY from '@/components/Category.vue'
+    import Category from '../components/Category.vue';
 
 </script>
 <template>
